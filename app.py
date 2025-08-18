08666e8445d11abb62edba989344bc86e4009958
lucasmsouza24 <lucasmesquita58@hotmail.com>
2025-08-18 01:10:10 -0300

    feat: :sparkles: calling `Header` in the `app.py` + remove trash code

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
-import pandas as pd
+from dash import Dash, html, page_container
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+from components.header import Header
 
-app = Dash()
+app = Dash(__name__, use_pages=True)
+app.title = 'Financial Dashboard'
+server = app.server
 
-app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
-    dcc.Dropdown(df.country.unique(), df.country.unique()[0], id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-]
-
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+app.layout = html.Div(
+    children=[
+        Header(),
+        page_container
+    ]
 )
-def update_graph(value):
-    dff = df[df.country == value]
-    return px.line(dff, x='year', y='pop')
 
 if __name__ == '__main__':
     app.run(debug=True)
