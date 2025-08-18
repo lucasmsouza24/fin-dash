fa76199cc60b68052b79cec3e672e965ad348422
lucasmsouza24 <lucasmesquita58@hotmail.com>
2025-08-18 01:52:53 -0300

    feat: :sparkles: add example callback to display graph

--- pages/home.py
+++ pages/home.py
@@ -1,10 +1,26 @@
-from dash import html, register_page
+from dash import html, dcc, register_page, Output, Input, callback
+import pandas as pd
+import plotly.express as px
 
 register_page(__name__, path="/")
+
+df = pd.read_csv('./data/data.csv')
 
 layout = html.Div(
     children=[
         html.H2("Financial Dashboard - Home"),
         html.P("Here, graphs will be displayed."),
+        dcc.Graph(
+            id='id-example-graph',
+            figure={}
+        )
     ]
 )
+
+# callbacks
+@callback(
+    Output('id-example-graph', 'figure'),
+    Input('period-filter', 'value')
+)
+def update_example_graph(_):
+    return px.bar(df, x='month', y='revenue')
